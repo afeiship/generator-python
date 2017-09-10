3e84d803cf4f2ad88d506974471dcf6ad814e953
afeiship <1290657123@qq.com>
2017-09-10 12:06:29 +0800

    issue: inital

---
+++ dist/index.js
@@ -0,0 +1,31 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+exports.default = function (inHttp, inConfig) {
+  var _this = this;
+
+  var _inOptions = inOptions,
+      APIS = _inOptions.APIS;
+
+  _nextJsCore2.default.each(APIS.items, function (key, item) {
+    _this[key] = function (inData) {
+      var action = String(item[0]).toLocaleLowerCase();
+      var apiPath = item[1];
+      if (apiPath.indexOf('{') === -1) {
+        apiPath = _nextJsCore2.default.tmpl(apiPath, inData);
+      }
+      return inHttp[action]('' + APIS.baseUrl + apiPath, inData);
+    };
+  });
+};
+
+var _nextJsCore = require('next-js-core2');
+
+var _nextJsCore2 = _interopRequireDefault(_nextJsCore);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import nx from 'next-js-core2';
+
+export default function(inHttp, inConfig){
+  const { APIS } = inOptions;
+  nx.each(APIS.items, (key, item) => {
+    this[key] = function (inData) {
+      const action = String(item[0]).toLocaleLowerCase();
+      let apiPath = item[1];
+      if(apiPath.indexOf('{')===-1){
+        apiPath = nx.tmpl( apiPath, inData );
+      }
+      return inHttp[action](`${APIS.baseUrl}${apiPath}`, inData);
+    };
+  });
+};
