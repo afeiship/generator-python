7c84e402e270d462726a3a2288a5a0ce7427ae28
afeiship <1290657123@qq.com>
2017-09-10 12:06:47 +0800

    issue: bugfix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default function(inHttp, inConfig){
     this[key] = function (inData) {
       const action = String(item[0]).toLocaleLowerCase();
       let apiPath = item[1];
-      if(apiPath.indexOf('{')===-1){
+      if(apiPath.indexOf('{') > -1){
         apiPath = nx.tmpl( apiPath, inData );
       }
       return inHttp[action](`${APIS.baseUrl}${apiPath}`, inData);
