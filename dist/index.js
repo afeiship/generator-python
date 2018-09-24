2e81f481db8484e1c0a10ae454b84116fb0f7fe1
afeiship <1290657123@qq.com>
2018-09-24 22:17:08 +0800

    issue: refactor to be done

--- dist/index.js
+++ dist/index.js
@@ -10,27 +10,58 @@ var _nextJsCore2 = _interopRequireDefault(_nextJsCore);
 
 require('next-tmpl');
 
+require('next-content-type');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var SEPARATOR = '{';
 
 function httpRestConfg(inApiContext, inHttp, inConfig) {
-  var items = inConfig.items,
-      baseUrl = inConfig.baseUrl;
+  var baseUrl = '//' + location.host;
+  var request = inConfig.request,
+      items = inConfig.items;
 
-  _nextJsCore2.default.each(items, function (context, apis) {
-    _nextJsCore2.default.each(apis, function (key, item) {
+
+  items.forEach(function (item) {
+    var _request = item.request;
+    var _items = item.items;
+
+    _nextJsCore2.default.each(_items, function (key, _item) {
       inApiContext[key] = function (inData, inOptions) {
         var data = Array.isArray(inData) ? _nextJsCore2.default.mix.apply(_nextJsCore2.default, inData) : inData;
-        var action = String(item[0]).toLocaleLowerCase();
-        var apiPath = item[1];
+        var action = String(_item[0]).toLowerCase();
+        var requestData = _request || request;
+        var context = requestData[0];
+        var contentType = _nextJsCore2.default.contentType(requestData[1]);
+        var options = _nextJsCore2.default.mix({
+          headers: {
+            'Content-Type': contentType
+          }
+        }, inOptions);
+
+        var apiPath = _item[1];
         if (apiPath.indexOf(SEPARATOR) > -1) {
           apiPath = _nextJsCore2.default.tmpl(apiPath, data);
         }
-        return inHttp[action]('' + baseUrl + context + apiPath, data, inOptions);
+
+        return inHttp[action]('' + baseUrl + context + apiPath, data, options);
       };
     });
   });
+
+  // nx.each(items, (context, apis) => {
+  //   nx.each(apis, (key, item) => {
+  //     inApiContext[key] = function (inData, inOptions) {
+  //       const data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
+  //       const action = String(item[0]).toLowerCase();
+  //       let apiPath = item[1];
+  //       if (apiPath.indexOf(SEPARATOR) > -1) {
+  //         apiPath = nx.tmpl(apiPath, data);
+  //       }
+  //       return inHttp[action](`${baseUrl}${context}${apiPath}`, data, inOptions);
+  //     };
+  //   })
+  // });
 };
 
 module.exports = httpRestConfg;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,53 @@
 import nx from 'next-js-core2';
 import 'next-tmpl';
+import 'next-content-type';
 
 const SEPARATOR = '{';
 
-function httpRestConfg(inApiContext, inHttp, inConfig){
-  const { items, baseUrl } = inConfig;
-  nx.each(items, (context, apis) => {
-    nx.each(apis,(key, item)=>{
+function httpRestConfg(inApiContext, inHttp, inConfig) {
+  const baseUrl = `//${location.host}`;
+  const { request, items } = inConfig;
+
+  items.forEach(function(item){
+    const _request = item.request;
+    const _items = item.items;
+
+    nx.each(_items, function (key, _item) {
       inApiContext[key] = function (inData, inOptions) {
         const data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
-        const action = String(item[0]).toLocaleLowerCase();
-        let apiPath = item[1];
-        if(apiPath.indexOf(SEPARATOR) > -1){
-          apiPath = nx.tmpl( apiPath, data );
+        const action = String(_item[0]).toLowerCase();
+        const requestData = _request || request;
+        const context = requestData[0];
+        const contentType = nx.contentType(requestData[1]);
+        const options = nx.mix({
+          headers: {
+            'Content-Type': contentType
+          }
+        }, inOptions);
+
+        let apiPath = _item[1];
+        if (apiPath.indexOf(SEPARATOR) > -1) {
+          apiPath = nx.tmpl(apiPath, data);
         }
-        return inHttp[action](`${baseUrl}${context}${apiPath}`, data, inOptions);
+
+        return inHttp[action](`${baseUrl}${context}${apiPath}`, data, options);
       };
     })
   });
+
+  // nx.each(items, (context, apis) => {
+  //   nx.each(apis, (key, item) => {
+  //     inApiContext[key] = function (inData, inOptions) {
+  //       const data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
+  //       const action = String(item[0]).toLowerCase();
+  //       let apiPath = item[1];
+  //       if (apiPath.indexOf(SEPARATOR) > -1) {
+  //         apiPath = nx.tmpl(apiPath, data);
+  //       }
+  //       return inHttp[action](`${baseUrl}${context}${apiPath}`, data, inOptions);
+  //     };
+  //   })
+  // });
 };
 
 
