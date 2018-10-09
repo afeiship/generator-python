7d38833a33c56ea5de97f12f05c869e545cb846c
afeiship <1290657123@qq.com>
2018-10-09 14:16:18 +0800

    issue: bugfix for dist file

--- dist/index.js
+++ dist/index.js
@@ -10,58 +10,25 @@ var _nextJsCore2 = _interopRequireDefault(_nextJsCore);
 
 require('next-tmpl');
 
-require('next-content-type');
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var SEPARATOR = '{';
 
 function httpRestConfg(inApiContext, inHttp, inConfig) {
-  var baseUrl = '//' + location.host;
-  var request = inConfig.request,
-      items = inConfig.items;
+  var items = inConfig.items,
+      baseUrl = inConfig.baseUrl;
 
-
-  items.forEach(function (item) {
-    var _request = item.request;
-    var _items = item.items;
-
-    _nextJsCore2.default.each(_items, function (key, _item) {
-      inApiContext[key] = function (inData, inOptions) {
-        var data = Array.isArray(inData) ? _nextJsCore2.default.mix.apply(_nextJsCore2.default, inData) : inData;
-        var action = String(_item[0]).toLowerCase();
-        var requestData = _request || request;
-        var context = requestData[0];
-        var contentType = _nextJsCore2.default.contentType(requestData[1]);
-        var options = _nextJsCore2.default.mix({
-          headers: {
-            'Content-Type': contentType
-          }
-        }, inOptions);
-
-        var apiPath = _item[1];
-        if (apiPath.indexOf(SEPARATOR) > -1) {
-          apiPath = _nextJsCore2.default.tmpl(apiPath, data);
-        }
-
-        return inHttp[action]('' + baseUrl + context + apiPath, data, options);
-      };
-    });
+  _nextJsCore2.default.each(items, function (key, item) {
+    inApiContext[key] = function (inData, inOptions) {
+      var data = Array.isArray(inData) ? _nextJsCore2.default.mix.apply(_nextJsCore2.default, inData) : inData;
+      var action = String(item[0]).toLocaleLowerCase();
+      var apiPath = item[1];
+      if (apiPath.indexOf(SEPARATOR) > -1) {
+        apiPath = _nextJsCore2.default.tmpl(apiPath, data);
+      }
+      return inHttp[action]('' + baseUrl + apiPath, data, inOptions);
+    };
   });
-
-  // nx.each(items, (context, apis) => {
-  //   nx.each(apis, (key, item) => {
-  //     inApiContext[key] = function (inData, inOptions) {
-  //       const data = Array.isArray(inData) ? nx.mix.apply(nx, inData) : inData;
-  //       const action = String(item[0]).toLowerCase();
-  //       let apiPath = item[1];
-  //       if (apiPath.indexOf(SEPARATOR) > -1) {
-  //         apiPath = nx.tmpl(apiPath, data);
-  //       }
-  //       return inHttp[action](`${baseUrl}${context}${apiPath}`, data, inOptions);
-  //     };
-  //   })
-  // });
 };
 
 module.exports = httpRestConfg;
