7d2f045b2f5d41f23fe867499c996304ae67b587
afeiship <1290657123@qq.com>
2018-10-10 10:04:23 +0800

    issue: udpate

--- dist/index.js
+++ dist/index.js
@@ -18,16 +18,18 @@ function httpRestConfg(inApiContext, inHttp, inConfig) {
   var items = inConfig.items,
       baseUrl = inConfig.baseUrl;
 
-  _nextJsCore2.default.each(items, function (key, item) {
-    inApiContext[key] = function (inData, inOptions) {
-      var data = Array.isArray(inData) ? _nextJsCore2.default.mix.apply(_nextJsCore2.default, inData) : inData;
-      var action = String(item[0]).toLocaleLowerCase();
-      var apiPath = item[1];
-      if (apiPath.indexOf(SEPARATOR) > -1) {
-        apiPath = _nextJsCore2.default.tmpl(apiPath, data);
-      }
-      return inHttp[action]('' + baseUrl + apiPath, data, inOptions);
-    };
+  _nextJsCore2.default.each(items, function (context, apis) {
+    _nextJsCore2.default.each(apis, function (key, item) {
+      inApiContext[key] = function (inData, inOptions) {
+        var data = Array.isArray(inData) ? _nextJsCore2.default.mix.apply(_nextJsCore2.default, inData) : inData;
+        var action = String(item[0]).toLocaleLowerCase();
+        var apiPath = item[1];
+        if (apiPath.indexOf(SEPARATOR) > -1) {
+          apiPath = _nextJsCore2.default.tmpl(apiPath, data);
+        }
+        return inHttp[action]('' + baseUrl + context + apiPath, data, inOptions);
+      };
+    });
   });
 };
 
